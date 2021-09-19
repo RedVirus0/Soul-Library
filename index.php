4a1027b1bf549ec0247009de6dea54d3dfd626e9
Jehad Alqurashi <anmar-v7@hotmail.com>
2021-09-19 21:38:04 +0300

    New Project

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'template/header.php';
+?>
+<html>
+
+    <head>
+        <title>Simple Library</title>
+    </head>
+
+    <body>
+
+    </body>
+
+
+</html>
+
---
+++ template/header.php
@@ -0,0 +1,35 @@
+<html>
+    <head>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-F3w7mX95PdgyTmZZMECAngseQB83DfGTowi0iMjiWaeVhAn4FJkqJByhZMI3AhiU" crossorigin="anonymous">
+        <link rel="stylesheet" href="css/header.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-/bQdsTh/da6pkI1MST/rWKFNjaCP5gBSY4sEBT38Q/9RBh9AH40zEOg7Hlq2THRZ" crossorigin="anonymous"></script>
+    </head>
+<body>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+        <a class="title" href="#">Soul Library</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarColor01">
+            <ul class="navbar-nav me-auto">
+                <li class="nav-item">
+                    <a style="margin-bottom:3px" class="nav-link active" href="#">الرئيسية
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#"></a>
+                </li>
+                <li class="nav-item">
+                    <a style="margin-bottom:3px" class="nav-link" href="#">تواصل معنا</a>
+                </li>
+            </ul>
+            <form class="d-flex" style="margin-top:10px" >
+                <input class="form-control me-sm-2" type="text" placeholder="اسم الكتاب">
+                <button class="btn btn-secondary my-2 my-sm-0" type="submit">بحث</button>
+            </form>
+        </div>
+    </div>
+</nav>
