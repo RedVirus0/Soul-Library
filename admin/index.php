b466aebcc334fb0a1ce3f9ab98b0d52075f47951
Jehad Alqurashi <anmar-v7@hotmail.com>
2021-09-20 15:51:17 +0300

    Edit Design index.php

--- admin/index.php
+++ admin/index.php
@@ -1,6 +1,8 @@
-<html>
+<html dir="ltr">
     <head>
         <title>لوحة التحكم</title>
+        <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
+
         <link rel="stylesheet" href="style/index.css">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
@@ -9,9 +11,10 @@
     <body>
         <div class="login">
             <h2 style="color:white;opacity: 1">لوحة التحكم</h2>
+
         <form class="forms" action="" method="post">
-            <input class="filed" type="text" name="email" placeholder="Email"><br>
-            <input class="filed" type="password" name="password" placeholder="Password" ><br>
+            <input class="filed" type="text" name="email" placeholder="البريد الألكتروني"><i class="fas fa-user"></i><br>
+            <input class="filed" type="password" name="password" placeholder="كلمة المرور" ><i class="fas fa-key"></i><br>
             <input class="submit" type="submit" value="دخول">
 
         </form>
