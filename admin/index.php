5091236159a45342692cbe4d73bc0bf7c0a3e653
Jehad Alqurashi <anmar-v7@hotmail.com>
2021-09-27 22:53:23 +0300

    add code php for login panel

--- admin/index.php
+++ admin/index.php
@@ -1,3 +1,21 @@
+<?php
+require '../config/database.php';
+session_start();
+if($_SERVER['REQUEST_METHOD'] == "POST"){
+    $email = mysqli_real_escape_string($connect,htmlspecialchars($_POST['email']));
+    $pass = mysqli_real_escape_string($connect,htmlspecialchars($_POST['password']));
+    $check = $connect->query("select email,password,role from users where email='$email' and password='$pass' and role='admin'");
+    if($check->num_rows){
+        $check=$check->fetch_assoc();
+        $_SESSION['role'] = $check['role'];
+        header("location:dashboard.php");
+    }else{
+        $fail = "<span style='color:red'>البريد الألكتروني او كلمة المرور خاطئه</span>";
+    }
+}
+
+
+?>
 <html dir="ltr">
     <head>
         <title>لوحة التحكم</title>
@@ -9,12 +27,18 @@
         <link href="https://fonts.googleapis.com/css2?family=Markazi+Text:wght@700&display=swap" rel="stylesheet">
     </head>
     <body>
+
         <div class="login">
+            <?php
+            if(isset($fail)){
+                echo $fail;
+            }
+            ?>
             <h2 style="color:white;opacity: 1">لوحة التحكم</h2>
 
         <form class="forms" action="" method="post">
-            <input class="filed" type="text" name="email" placeholder="البريد الألكتروني"><i class="fas fa-user"></i><br>
-            <input class="filed" type="password" name="password" placeholder="كلمة المرور" ><i class="fas fa-key"></i><br>
+            <input id="email" class="filed" type="text" name="email" placeholder="البريد الألكتروني"><i class="fas fa-user"></i><br>
+            <input id="pass" class="filed" type="password" name="password" placeholder="كلمة المرور" ><i class="fas fa-key"></i><br>
             <input class="submit" type="submit" value="دخول">
 
         </form>
@@ -22,3 +46,4 @@
     </body>
 
 </html>
+<script src="js/index.js"></script>
